80af4b38ddda3375647bd17adf47a2a196b2c1ba
吾与谁归 <55055181+timenodes@users.noreply.github.com>
2024-11-30 13:41:46 +0800

    Add files via upload

---
+++ ChatProView.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using MAIModel.ViewModels;
+using System.Windows.Controls;
+
+namespace MAIModel.Views
+{
+    /// <summary>
+    /// ChatView2.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChatProView : UserControl
+    {
+        ChatProViewModel viewModel;
+        public ChatProView(ShareOllamaObject shareOllama)
+        {
+            InitializeComponent();
+            viewModel = new ChatProViewModel();
+            viewModel.SetOllama(shareOllama);
+            this.DataContext = viewModel;
+
+        }
+    }
+}
