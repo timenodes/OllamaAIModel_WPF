cc42069cdd274025b6abc81b23e333466c24cad9
吾与谁归 <55055181+timenodes@users.noreply.github.com>
2024-11-30 14:10:54 +0800

    Add files via upload

---
+++ MAIModel/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+[assembly: ThemeInfo(
+    //特定于主题的资源字典所在的位置（如果在页面或应用程序资源字典中找不到资源时使用）
+    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
+                                                //(used if a resource is not found in the page,
+                                                // or application resource dictionaries)
+
+   //通用资源字典所在的位置（如果在页面、应用程序或任何特定于主题的资源字典中找不到资源时使用）
+    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
+                                                //(used if a resource is not found in the page,
+                                                // app, or any theme specific resource dictionaries)
+)]
