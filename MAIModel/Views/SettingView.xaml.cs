f21c675907acf25545e41f1ca44136518f6236dd
吾与谁归 <55055181+timenodes@users.noreply.github.com>
2024-11-30 14:24:06 +0800

    Add files via upload

---
+++ MAIModel/Views/SettingView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MAIModel.ViewModels;
+using System.Windows.Controls;
+
+namespace MAIModel.Views
+{
+    /// <summary>
+    /// SettingView.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingView : UserControl
+    {
+        SettingViewModel _viewModel;
+        public SettingView(ShareOllamaObject ollama)
+        {
+            InitializeComponent();
+            _viewModel = new SettingViewModel(ollama);
+            this.DataContext = _viewModel;
+        }
+    }
+}
